7980729e2e39db8e41db290eed17abe035ad28a6
Maryna Vyshnevska <vyshnevska.maryna@gmail.com>
2022-12-25 13:35:02 +0200

    task1 13-34

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,40 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import React, { Component } from "react";
+// import PropTypes from 'prop-types';
+
+import FeedbackOptions from "./FeedbackOptions";
+// import Statistics from "./Statistics";
+// import Section from "./Section";
+
+export class App extends Component {
+  state = {
+    good: 0,
+    netural: 0,
+    bad: 0
+  }
+
+  leaveFeedback = event => {
+    const type = event.target.textContent;
+    this.setState(prevState => {
+      return ({
+        [type]: prevState[type] + 1,
+      });
+    });
+  };
+  render() {
+    // const { good, neutral, bad } = this.state;
+    const feedbackOptions = Object.keys(this.state);
+
+    return (
+      // <Section title="Please leave feedback">
+        <FeedbackOptions 
+          options={feedbackOptions}
+          onLeaveFeedback={this.leaveFeedback}  />
+      // </Section>
+      // <Section title="Statistics">
+        // <Statistics />
+      // </Section>
+    )  
+  }
 };
+
+
